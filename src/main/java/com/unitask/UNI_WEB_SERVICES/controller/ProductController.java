add19b45c867034c6c516f755a704ad08bccbc1d
gtenov <tenov@media-it-services.de>
2024-09-25 19:56:48 +0300

    Adding my sql database and jpa

--- src/main/java/com/unitask/UNI_WEB_SERVICES/controller/ProductController.java
+++ src/main/java/com/unitask/UNI_WEB_SERVICES/controller/ProductController.java
@@ -1,8 +1,6 @@
 package com.unitask.UNI_WEB_SERVICES.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
